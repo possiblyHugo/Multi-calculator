9701926519b1a1ae3602ed6d5af0a114889851c4
possiblyHugo <alainhandzar@gmail.com>
2024-03-04 05:21:45 -0800

    + Initial prompt

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+	int calculator_selection = 0;
+
+	std::cout << "This program is meant to serve as a multi-purpose calculator that helps with understanding the step by step solving process of problems." << std::endl;
+	std::cout << "Please select the calculator from the list: ";
+
+	return 0;
+}
